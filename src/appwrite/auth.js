b6eb8e8941387f4ae01a65db70015063d3079ddc
parashardharmik3 <parashardharmik3@gmail.com>
2024-06-22 15:19:32 +0530

    first commit

---
+++ src/appwrite/auth.js
@@ -0,0 +1,61 @@
+import conf from '../conf/conf.js';
+import { Client, Account, ID } from "appwrite";
+
+export class AuthService {
+    client = new Client();
+    account;
+
+    constructor() {
+        this.client
+            .setEndpoint(conf.appwriteUrl)
+            .setProject(conf.appwriteProjectId);
+        this.account = new Account(this.client);
+    }
+
+    async createAccount({ email, password, name }) {
+        try {
+            const userAccount = await this.account.create(ID.unique(), email, password, name);
+            if (userAccount) {
+                // Call another method to create a session after account creation
+                return this.login({ email, password });
+            } else {
+                return userAccount;
+            }
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async login({ email, password }) {
+        try {
+            const session = await this.account.createEmailSession(email, password);
+            if (session) {
+                // If login is successful, you can fetch the current user
+                return await this.getCurrentUser();
+            }
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async getCurrentUser() {
+        try {
+            return await this.account.get();
+        } catch (error) {
+            console.log("Appwrite service :: getCurrentUser :: error", error);
+            throw error;  // Re-throw the error to handle it in the calling function
+        }
+    }
+
+    async logout() {
+        try {
+            await this.account.deleteSessions();
+        } catch (error) {
+            console.log("Appwrite service :: logout :: error", error);
+        }
+    }
+}
+
+const authService = new AuthService();
+
+export default authService;
